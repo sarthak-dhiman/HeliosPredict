17df8078430cd420c025e13e23accde7844e260c
Sarthak Dhiman <93830724+sarthak-dhiman@users.noreply.github.com>
2026-01-09 17:07:04 +0530

    updated server.py

--- app/server.py
+++ app/server.py
@@ -1,24 +1,30 @@
 import os, joblib
-from pathlib import Path
 import pandas as pd
+from pathlib import Path
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-TF_MODEL = Path("models/best_model_nn.keras")
-PREPROC = Path("models/preprocessor.joblib")
-SK_MODEL = Path("models/best_model.joblib")
+from tensorflow import keras
 
+# Paths
+RF_MODEL = Path("models/rf_model.joblib")
+NN_MODEL = Path("models/nm_model.h5")
+PREPROC = Path("models/preprocessor.joblib")
+
+# Load preprocessor
 preproc = joblib.load(PREPROC)
 
-if TF_MODEL.exists():
-    from tensorflow import keras
-    model = keras.models.load_model(TF_MODEL, compile=False)
+# Load model
+if NN_MODEL.exists():
+    model = keras.models.load_model(NN_MODEL, compile=False)
     model_type = "nn"
+elif RF_MODEL.exists():
+    model = joblib.load(RF_MODEL)
+    model_type = "rf"
 else:
-    import joblib as jl
-    model = jl.load(SK_MODEL)
-    model_type = "sklearn"
+    raise RuntimeError("No model file found in /models")
 
+# Input schema
 class WeatherIn(BaseModel):
     uv_index: float
     condition: str
@@ -40,8 +46,10 @@ def health():
 def predict(x: WeatherIn):
     X = pd.DataFrame([x.model_dump()])
     Xp = preproc.transform(X)
+
     if model_type == "nn":
         y = float(model.predict(Xp, verbose=0)[0][0])
     else:
         y = float(model.predict(Xp)[0])
+
     return {"predicted_power_kw": round(y, 3)}
